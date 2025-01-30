e6616f3124284ec592356b88a8e850b304e8c02a
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-29 23:11:45 -0800

    mongo connected

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,22 +1,19 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://haroutyunhaltunyan93:rU2AozZmkagXt7be@cluster0.7scgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+module.exports = mongoose.connection;
+
+
+
+
+
 // const mongoose = require('mongoose');
+// require('dotenv').config(); // Load .env file
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://haroutyunhaltunyan93:rU2AozZmkagXt7be@cluster0.7scgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mydatabase';
 
-// module.exports = mongoose.connection;
-
-
-
-
-// const mongoose = require('mongoose');
-// require('dotenv').config(); // Load environment variables from .env
-
-// // Use the environment variable or fallback to localhost (for development)
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mydatabase";
-
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// mongoose.connect(MONGODB_URI);
 
 // const db = mongoose.connection;
 
@@ -24,21 +21,3 @@
 // db.once('open', () => console.log('✅ Connected to MongoDB! 🚀'));
 
 // module.exports = db;
-
-
-
-
-
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load .env file
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mydatabase';
-
-mongoose.connect(MONGODB_URI);
-
-const db = mongoose.connection;
-
-db.on('error', (err) => console.error('❌ MongoDB connection error:', err));
-db.once('open', () => console.log('✅ Connected to MongoDB! 🚀'));
-
-module.exports = db;

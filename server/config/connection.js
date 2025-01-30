80485a17458f1e2bb9a29c091330874d5b6e87a0
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-30 10:34:07 -0800

    mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb+srv://haroutyunhaltunyan93:rU2AozZmkagXt7be@cluster0.7scgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-)
-.then(() => console.log('MongoDB connected successfully'))
-.catch((err) => console.error('MongoDB connection error:', err));
+  )
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
 

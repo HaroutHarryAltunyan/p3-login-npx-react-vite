6c2163ddc5033391fac0c45b365c7faf03076ba7
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2025-01-30 11:05:51 -0800

    'client/dist/index.html'));

--- server/server.js
+++ server/server.js
@@ -79,9 +79,9 @@ const startApolloServer = async () => {
 
   // Serve up static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    app.use(express.static(path.join(__dirname, 'client/dist')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, 'client/dist/index.html'));
     });
   }
 
